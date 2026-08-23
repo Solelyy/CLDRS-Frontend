494c1bed5d4a2aba7e26eb06758187d510bf7575
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2026-08-23 16:28:17 +0800

    update

---
+++ app/routes/_protected.tsx
@@ -0,0 +1,36 @@
+//this is the guard for authentication
+import { Outlet, redirect } from "react-router";
+import type { Route } from ".react-router/types/app/+types/root";
+
+import { getAuthUser } from "~/lib/server/getUserSession";
+import { userContext } from "~/lib/server/userContext";
+
+export const route = {
+  path: "", 
+};
+
+// auth middleware
+async function authMiddleware({ request, context }: any) {
+    const user = await getAuthUser(request);
+
+    if (!user) {
+        throw redirect("/unauthorized");
+    }
+
+    // make user available to children
+    context.set(userContext, user);
+}
+
+// apply middleware to all child routes
+export const middleware = [authMiddleware];
+
+// optional loader (useful later if you want layout UI like header)
+export async function loader({ context }: Route.LoaderArgs) {
+    const user = context.get(userContext);
+    return { user };
+}
+
+// layout wrapper
+export default function ProtectedLayout() {
+    return <Outlet />;
+}
